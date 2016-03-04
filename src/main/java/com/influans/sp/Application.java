8f792f26ef191a33023007b9e660571b321f88b2
Hazem Ben Khalfallah <hazem.benkhalfallah@influans.com>
2016-03-04 21:36:29 +0100

    fix build

--- src/main/java/com/influans/sp/Application.java
+++ src/main/java/com/influans/sp/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 
